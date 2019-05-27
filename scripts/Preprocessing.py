ff9d64e4fd00efbf675856a2cd2fdef70cf252c1
kcoffland <kyle.coffland@gmail.com>
2019-05-27 17:32:26 -0500

    Added the ability to filter columns out if the user wanted

--- scripts/Preprocessing.py
+++ scripts/Preprocessing.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 class Preprocessing:
     """
@@ -7,19 +7,35 @@ class Preprocessing:
     columns will be filled with 0s to give no additional information).
     """
 
-    def __init__(self):
+    def __init__(self, cols_to_filter=None):
+        """
+        Args:
+            cols_to_filter: list of strings, names columns which need
+                            to be removed from the dataset
+        """
+
+        # stores whether the model processor has been fit to the data
         self.is_fit = False
+
+        assert(type(cols_to_filter) == list)
+        self.cols_to_filter = cols_to_filter
 
     def fit(self, X, y=None):
         """
         Finds out what which columns need to be converted to dummy
         values and stores it as an attribute to be used later in
-        the transform method
+        the transform method.
         """
         self.is_fit = True
 
+        # Defined here instead of __init__ because the constructor
+        # won't know the data being passed in, and I don't want to
+        # make unnecessary assignments. I've ensured in the rest of
+        # the methods that fit must be called before the rest
         self.categorical_cols = pd.Index([x for x in X.columns
-                                            if X[x].dtype == object]
+                                            if X[x].dtype == object \
+                                                and x not in self.cols_to_filter
+                                          ]
                                    )
 
         return self
@@ -29,6 +45,11 @@ class Preprocessing:
         # Ensuring data has been fit before this method can be used
         if not self.is_fit:
             raise Error("Fit method must be called before data can be transformed")
+
+        # Removing columns that the user wants to filter
+        for col in self.cols_to_filter:
+            assert(col in X.columns and "Given filter column not in data")
+        X.drop(self.cols_to_filter, inplace=True, axis=1)
 
         # Setting all values for columns not in the training data to 0
         # so no additional information can be learned
