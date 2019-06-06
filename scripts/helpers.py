e069d692420283f73b08a3961668c1443fa7412a
kcoffland <kyle.coffland@gmail.com>
2019-06-06 11:53:48 -0500

    Added functions to test whether the instance requires college, then added a function to combine this knowledge with Experience level binning

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -37,8 +37,66 @@ def mean_squared_error(predictions, actual):
 
     return ((predictions-actual)**2).sum() / len(actual)
 
-        
-        
+def find_outliers(data):
+    """Finds the values for which outliers begin based on 1.5*iqr
+    Args:
+        data: Series, holds data you want to find the outliers for
+    Returns:
+        lower, upper: floats, values less than lower are outliers and
+                              values larger than upper are outliers
+    """
+
+    # Finding the interquartile range
+    q1 = data.quantile(.25)
+    q3 = data.quantile(.75)
+    iqr = q3-q1
+
+    upper = q3 + iqr*1.5
+    lower = q1 - iqr*1.5
+
+    return lower, upper
+
+def attended_college(df):
+    """Tests the given dataframe to see if the instance requires college
+        Function is specific to the salary-predictions project
+    Args:
+        df: DataFrame, holds features that the function will be applied to
+    Returns:
+        bool, True if the feature needs college and False if it doesn't
+    """
+
+    if df.degree == 'NONE' or df.degree == 'HIGH_SCHOOL':
+        return False
+    else:
+        return True
+
+def grad_types(df):
+    """Determines the grad type of the instance
+        Function is specific to salary-predictions project
+        This must be run after attended_college
+    Args:
+        df: DataFrame, holds the instances we want to test
+    Returns:
+        string, Denotes Education and experience level
+    """
+
+    assert('college' in df.columns)
+    assert('ExperienceLevel' in df.columns)
+
+    if df['college']:
+        if df['ExperienceLevel'] == 0:
+            return 'No College, Inexperienced'
+        elif df['ExperienceLevel'] == 1:
+            return 'No College, Some Real Experience'
+        else:
+            return 'No College, Real World Experienced'
+    else:
+        if df['ExperienceLevel'] == 0:
+            return 'Recent Grad'
+        elif df['ExperienceLevel'] == 1:
+            return 'Some Experience Grad'
+        else:
+            return 'Experienced Grad'
         
         
         
