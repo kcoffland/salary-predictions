7948213711c17e36e3ae4ce36aac71d64151d10f
kcoffland <kyle.coffland@gmail.com>
2019-06-06 21:11:42 -0500

    Added functionality to bin columns and engineer the gradType feature

--- scripts/Preprocessing.py
+++ scripts/Preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from helpers import attended_college, grad_types
 
 class Preprocessing:
     """
@@ -8,21 +9,55 @@ class Preprocessing:
     columns will be filled with 0s to give no additional information).
     """
 
-    def __init__(self, cols_to_filter=None):
+    def __init__(self, cols_to_filter=None, ordinal_cols=None, binned_cols=None,
+                 grad_types=False):
         """
         Args:
             cols_to_filter: list of strings, names columns which need
                             to be removed from the dataset
+            ordinal_cols: dict {string: list of strings}, the key is the ordinal
+                            column and the value is the list of categories in
+                            order of importance. ORDER OF IMPORTANCE WILL MATTER
+            binned_cols: dict {string: int}, the key is the column which
+                            the user wants to be binned. The int will be the number of
+                            bins that will be created.The data will be binned into equal
+                            quantiles using pd.qcut(). The new column will be added to
+                            the dataframe as 'given_binned' where "given" is the key of
+                            the dict. If the user wants the original column removed, then
+                            they can add it to the columns to filter. Note: some engineered
+                            features require the binning of certain columns and will
+                            not work if they have not been binned.
+            grad_types: bool, if True, then the grad_types feature will be created.
+                            ONLY TO BE USED FOR SALARY-PREDICTIONS
         """
 
         # stores whether the model processor has been fit to the data
         self.is_fit = False
 
-        assert(type(cols_to_filter) == list)
+        if cols_to_filter:
+            assert(type(cols_to_filter) == list)
+
         self.cols_to_filter = cols_to_filter
 
+        # Setting up ordinal columns encoding
+        self.ordinal_cols = ordinal_cols
+        if ordinal_cols:
+            assert(type(ordinal_cols) == dict)
+            categories = [self.ordinal_cols[x] for x in ordinal_cols.keys()]
+            self.ordinal_enc = OrdinalEncoder(categories=categories)
+        else:
+            self.ordina_enc = None
+
         # scaler to make numerical columns between 0 and 1
-        self.scaler = MinMaxScaler()
+        self.min_max_scaler = MinMaxScaler()
+
+        # Setting up binned columns
+        self.binned_cols = binned_cols
+        if self.binned_cols:
+            self.bins = [[] for i in range(len(self.binned_cols))]
+
+        self.grad_types = grad_types
+
 
     def fit(self, X, y=None):
         """
@@ -32,47 +67,106 @@ class Preprocessing:
         """
         self.is_fit = True
 
+        # Creating the bins from the training data to cut on later
+        if self.binned_cols:
+            for i, col in enumerate(self.binned_cols.keys()):
+                assert (col in X.columns)
+                column, self.bins[i] = pd.qcut(X[col], self.binned_cols[col],
+                                            labels=False, retbins=True)
+
         # Defined here instead of __init__ because the constructor
         # won't know the data being passed in, and I don't want to
         # make unnecessary assignments. I've ensured in the rest of
         # the methods that fit must be called before the rest
-        self.categorical_cols = pd.Index([x for x in X.columns
-                                            if X[x].dtype == object \
-                                                and x not in self.cols_to_filter
-                                          ]
-                                   )
 
-        self.numerical_cols = pd.Index([x for x in X.columns
-                                            if x not in self.categorical_cols \
-                                                and x not in self.cols_to_filter
-                                          ]
-                                   )
+        if self.cols_to_filter:
+            self.categorical_cols = pd.Index([x for x in X.columns
+                                                if X[x].dtype == object \
+                                                    and x not in self.cols_to_filter
+                                              ]
+                                       )
+
+            self.numerical_cols = pd.Index([x for x in X.columns
+                                                if x not in self.categorical_cols \
+                                                    and x not in self.cols_to_filter
+                                              ]
+                                       )
+        else:
+            self.categorical_cols = pd.Index([x for x in X.columns
+                                              if X[x].dtype == object
+                                              ]
+                                             )
+
+            self.numerical_cols = pd.Index([x for x in X.columns
+                                            if x not in self.categorical_cols
+                                            ]
+                                           )
+
+        if self.ordinal_cols:
+            for col in self.ordinal_cols.keys():
+                assert(col in X.columns and f"Given ordinal column {col} not in X")
+            # I need to ensure
+            else:
+                self.ordinal_enc.fit(X[list(self.ordinal_cols.keys())])
+
 
         # Fitting the scaler to the training data's numerical columns
-        self.scaler.fit(X[self.numerical_cols])
+        self.min_max_scaler.fit(X[self.numerical_cols])
 
         return self
 
     def transform(self, X, y=None):
+        # maybe this naming convention is why software engineers hate Data Scientists
+        X_new = X.copy()
 
         # Ensuring data has been fit before this method can be used
         if not self.is_fit:
             raise Error("Fit method must be called before data can be transformed")
 
-        # Removing columns that the user wants to filter
-        for col in self.cols_to_filter:
-            assert(col in X.columns and "Given filter column not in data")
-        X.drop(self.cols_to_filter, inplace=True, axis=1)
-
         # Setting all values for columns not in the training data to 0
-        # so no additional information can be learned
-        new_cols = set(X.columns) - set(self.categorical_cols) - set(self.numerical_cols)
-
+        # so no additional information can be learned outside of engineering
+        # new features available in the training data
+        new_cols = set(X_new.columns) - set(self.categorical_cols) - \
+                   set(self.numerical_cols)
         for col in new_cols:
-            X[col] = 0
+            X_new[col] = 0
 
-        X_new = pd.get_dummies(X)
-        X_new[self.numerical_cols] = self.scaler.transform(X_new[self.numerical_cols])
+        ################ Creating/Removing Columns ##################
+        # Binning appropriate columns
+        if self.binned_cols:
+            for i, col in enumerate(self.binned_cols.keys()):
+                # I cast to int so that I don't have to worry about the Category dtype
+                X_new[f'{col}_binned'] = pd.cut(X_new[col], self.bins[i],)\
+                                            .astype(int).copy()
+                X_new[f'{col}_binned'] /= X_new[f'{col}_binned'].max()
+
+        # Adding any new features
+        if self.grad_types:
+            assert('yearsExperience' in self.binned_cols.keys())
+            X_new['attendedCollege'] = X_new.apply(attended_college, axis=1)
+            X_new['gradTypes'] = X_new.apply(grad_types, axis=1)
+            # Scaling it here because it won't be in the numerical_cols
+            X_new['gradTypes'] /= X_new['gradTypes'].max()
+
+        # Removing columns that the user wants to filter
+        if self.cols_to_filter:
+            for col in self.cols_to_filter:
+                assert(col in X_new.columns and f"Given column {col} not in data")
+            X_new.drop(self.cols_to_filter, inplace=True, axis=1)
+
+        ################### Transforming Data ########################
+        # Encoding Ordinal columns as well as ensuring they're scaled properly
+        if self.ordinal_cols:
+            X_new[list(self.ordinal_cols.keys())] = self.ordinal_enc.transform(
+                                                        X_new[list(self.ordinal_cols.keys())]
+                                                    )
+            X_new[list(self.ordinal_cols.keys())] /= X_new[list(self.ordinal_cols.keys())].max()
+
+        # Dummy the rest of the categorical variables
+        X_new = pd.get_dummies(X_new)
+
+        # Scale the numerical columns
+        X_new[self.numerical_cols] = self.min_max_scaler.transform(X_new[self.numerical_cols])
 
         return X_new
 
